9ab7fd83fed85331488367dd92e93dfca4114354
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2025-05-23 10:30:32 +0200

    d

--- engine/minimap_bonus.c
+++ engine/minimap_bonus.c
@@ -6,106 +6,85 @@
 /*   By: alix <alix@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 05:15:00 by aconstan          #+#    #+#             */
-/*   Updated: 2025/05/23 09:56:27 by alix             ###   ########.fr       */
+/*   Updated: 2025/05/23 10:30:23 by alix             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
 
-static int	minimap_get_color(char c)
+static void	minimap_draw_square(t_img_data *img, int x, int y, int color)
 {
-	if (c == '1')
-		return (WALL_COLOR);
-	if (c == '0')
-		return (FLOOR_COLOR);
-	return (VOID_COLOR);
-}
-
-static void	minimap_draw_square(t_minimap *ctx, int px, int py, int color)
-{
-	int		i;
-	int		j;
+	int		px;
+	int		py;
 	char	*dst;
 
-	i = 0;
-	while (i < ctx->scale)
+	int i, j;
+	for (i = 0; i < MINIMAP_SCALE; i++)
 	{
-		j = 0;
-		while (j < ctx->scale)
+		for (j = 0; j < MINIMAP_SCALE; j++)
 		{
-			if (px + i >= 0 && px + i < ctx->img->width && py + j >= 0 && py
-				+ j < ctx->img->height)
+			px = x + i;
+			py = y + j;
+			if (px >= 0 && px < img->width && py >= 0 && py < img->height)
 			{
-				dst = ctx->img->addr + ((py + j) * ctx->img->line_len + (px + i)
-						* (ctx->img->bpp / 8));
+				dst = img->addr + (py * img->line_len + px * (img->bpp / 8));
 				*(unsigned int *)dst = color;
 			}
-			j++;
 		}
-		i++;
-	}
-}
-
-static void	minimap_draw_cells(t_config *cfg, t_minimap *ctx)
-{
-	int	mx;
-	int	my;
-
-	my = 0;
-	while (my < cfg->map.height)
-	{
-		mx = 0;
-		while (mx < cfg->map.width)
-		{
-			minimap_draw_square(ctx, ctx->origin_x + mx * ctx->scale,
-				ctx->origin_y + my * ctx->scale,
-				minimap_get_color(cfg->map.map[my][mx]));
-			mx++;
-		}
-		my++;
-	}
-}
-
-static void	minimap_draw_cursor(t_config *cfg, t_minimap *ctx)
-{
-	int	px;
-	int	py;
-	int	i;
-	int	j;
-
-	px = ctx->origin_x + (int)(cfg->player.pos_x * ctx->scale);
-	py = ctx->origin_y + (int)(cfg->player.pos_y * ctx->scale);
-	i = -3;
-	while (i <= 3)
-	{
-		j = -3;
-		while (j <= 3)
-		{
-			minimap_draw_square(ctx, px + i, py + j, CURSOR_COLOR);
-			j++;
-		}
-		i++;
 	}
 }
 
 void	draw_minimap(t_config *cfg)
 {
-	t_minimap	ctx;
-	int			scale_x;
-	int			scale_y;
-	int			mh;
+	t_map_data *map = &cfg->map;
+	t_img_data *img = &cfg->win;
+	int map_x, map_y, color;
 
-	ctx.img = &cfg->win;
-	scale_x = (cfg->win.width / 4) / cfg->map.width;
-	scale_y = (cfg->win.height / 4) / cfg->map.height;
-	ctx.scale = scale_x;
-	if (scale_y < scale_x)
-		ctx.scale = scale_y;
-	if (ctx.scale < 3)
-		ctx.scale = 3;
-	mh = cfg->map.height * ctx.scale;
-	ctx.origin_x = MINIMAP_MARGIN;
-	ctx.origin_y = ctx.img->height - mh - MINIMAP_MARGIN;
-	minimap_draw_cells(cfg, &ctx);
-	minimap_draw_cursor(cfg, &ctx);
+	int max_width = img->width / 4;
+	int max_height = img->height / 4;
+	int scale_x = max_width / map->width;
+	int scale_y = max_height / map->height;
+	int minimap_scale = (scale_x < scale_y) ? scale_x : scale_y;
+	if (minimap_scale < 3) minimap_scale = 3;
+
+	int minimap_height = map->height * minimap_scale;
+	int origin_x = MINIMAP_MARGIN;
+	int origin_y = img->height - minimap_height - MINIMAP_MARGIN;
+
+	for (map_y = 0; map_y < map->height; map_y++)
+	{
+		for (map_x = 0; map_x < map->width; map_x++)
+		{
+			char cell = map->map[map_y][map_x];
+			if (cell == '1')
+				color = 0x00755428; // mur marron
+			else if (cell == '0')
+				color = 0x00EDD8B0; // sol beige
+			else
+				color = 0x00FFFFFF; // vide/blanc
+
+			minimap_draw_square(img,
+				origin_x + map_x * minimap_scale,
+				origin_y + map_y * minimap_scale,
+				color);
+		}
+	}
+
+	// Curseur joueur ROUGE, carré plein et uniforme
+	int cursor_radius = 3;
+	int px = origin_x + (int)(cfg->player.pos_x * minimap_scale);
+	int py = origin_y + (int)(cfg->player.pos_y * minimap_scale);
+	for (int i = -cursor_radius; i <= cursor_radius; i++)
+		for (int j = -cursor_radius; j <= cursor_radius; j++)
+		{
+			int x = px + i;
+			int y = py + j;
+			if (x >= 0 && x < img->width && y >= 0 && y < img->height)
+			{
+				char *dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
+				*(unsigned int *)dst = 0x00FF2222;
+			}
+		}
 }
+
+
