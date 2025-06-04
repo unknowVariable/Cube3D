9b6c34ea369bd44fcf1ee6e120b394921b2bddd7
aconstan <64416579+alixconstantin@users.noreply.github.com>
2025-06-04 14:50:01 +0200

    debbugDDA

--- engine/dda_bonus.c
+++ engine/dda_bonus.c
@@ -6,7 +6,7 @@
 /*   By: alix <alix@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 05:15:00 by aconstan          #+#    #+#             */
-/*   Updated: 2025/06/03 21:22:29 by alix             ###   ########.fr       */
+/*   Updated: 2025/06/04 14:49:44 by alix             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,37 +68,53 @@ void	loop_dda(t_config *conf, t_ray *ray)
 	}
 }
 
-double	perform_dda(t_config *conf, t_ray *ray)
+double perform_dda(t_config *conf, t_ray *ray)
 {
-	int	hit;
-	t_door *door;
+    int hit = 0;
+    t_door *door;
 
-	hit = 0;
-	while (!hit)
-	{
-		if (ray->side_dist_x < ray->side_dist_y)
-		{
-			ray->side_dist_x += ray->delta_x;
-			ray->map_x += ray->step_x;
-			ray->side = 0;
-		}
-		else
-		{
-			ray->side_dist_y += ray->delta_y;
-			ray->map_y += ray->step_y;
-			ray->side = 1;
-		}
-		if (conf->map.map[ray->map_y][ray->map_x] == 'D')
-		{
-			door = get_door_at(conf, ray->map_x, ray->map_y);
-			if (door && !door->is_open)
-				hit = 1;
-		}
-		else if (conf->map.map[ray->map_y][ray->map_x] == '1')
-			hit = 1;
-	}
-	if (ray->side == 0)
-		return (ray->side_dist_x - ray->delta_x);
-	else
-		return (ray->side_dist_y - ray->delta_y);
+    while (!hit)
+    {
+        // 1. Vérifie d'abord les limites de la map
+        if (ray->map_x < 0 || ray->map_x >= conf->map.width ||
+            ray->map_y < 0 || ray->map_y >= conf->map.height)
+        {
+            hit = 1; // Sortie si hors limites
+            break;
+        }
+
+        // 2. Logique DDA originale
+        if (ray->side_dist_x < ray->side_dist_y)
+        {
+            ray->side_dist_x += ray->delta_x;
+            ray->map_x += ray->step_x;
+            ray->side = 0;
+        }
+        else
+        {
+            ray->side_dist_y += ray->delta_y;
+            ray->map_y += ray->step_y;
+            ray->side = 1;
+        }
+
+        // 3. Vérifie le contenu de la case (SEULEMENT si dans les limites)
+        if (ray->map_x >= 0 && ray->map_x < conf->map.width &&
+            ray->map_y >= 0 && ray->map_y < conf->map.height)
+        {
+            if (conf->map.map[ray->map_y][ray->map_x] == 'D')
+            {
+                door = get_door_at(conf, ray->map_x, ray->map_y);
+                if (door && !door->is_open)
+                    hit = 1;
+            }
+            else if (conf->map.map[ray->map_y][ray->map_x] == '1')
+                hit = 1;
+        }
+    }
+
+    // Calcul de la distance
+    if (ray->side == 0)
+        return (ray->side_dist_x - ray->delta_x);
+    else
+        return (ray->side_dist_y - ray->delta_y);
 }
