212041f8ace36a7e52e2947b52a8a70f0c112dd7
aconstan <64416579+alixconstantin@users.noreply.github.com>
2025-06-04 20:58:47 +0200

    dwadw

--- engine/render_utils.c
+++ engine/render_utils.c
@@ -6,7 +6,7 @@
 /*   By: alix <alix@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 05:15:00 by aconstan          #+#    #+#             */
-/*   Updated: 2025/06/04 20:51:25 by alix             ###   ########.fr       */
+/*   Updated: 2025/06/04 20:57:46 by alix             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void put_pixel(t_config *conf, int x, int y, int color)
 {
     char *dst;
     
-    if (x < 0 || x >= WIN_WIDTH || y < 0 || y >= WIN_HEIGHT)
+    if (!conf || !conf->win.addr || x < 0 || x >= WIN_WIDTH || y < 0 || y >= WIN_HEIGHT)
         return;
-        
     dst = conf->win.addr + (y * conf->win.line_len + x * (conf->win.bpp / 8));
-    *(unsigned int*)dst = color;
+    if (dst >= conf->win.addr && dst < conf->win.addr + (WIN_HEIGHT * conf->win.line_len))
+        *(unsigned int*)dst = color;
 }
