9be4d674ac3752bd1a20f1d72108275ec16b110a
aconstan <64416579+alixconstantin@users.noreply.github.com>
2025-06-04 19:34:05 +0200

    newMap

--- engine/render_utils.c
+++ engine/render_utils.c
@@ -6,20 +6,16 @@
 /*   By: alix <alix@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 05:15:00 by aconstan          #+#    #+#             */
-/*   Updated: 2025/06/04 14:57:39 by alix             ###   ########.fr       */
+/*   Updated: 2025/06/04 19:34:00 by alix             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
 
-void put_pixel(t_config *conf, int x, int y, int color)
+void	put_pixel(t_config *conf, int y, int color)
 {
-    char *dst;
+	int	offset;
 
-    // Vérifications cruciales
-    if (!conf || !conf->win.addr || x < 0 || x >= WIN_WIDTH || y < 0 || y >= WIN_HEIGHT)
-        return;
-
-    dst = conf->win.addr + (y * conf->win.line_len + x * (conf->win.bpp / 8));
-    *(unsigned int*)dst = color;
+	offset = y * conf->win.line_len + conf->win.x * (conf->win.bpp / 8);
+	*(unsigned int *)(conf->win.addr + offset) = color;
 }
