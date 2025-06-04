f58239f6fe7c3d56f4304566a993f2e18e70fd7f
aconstan <64416579+alixconstantin@users.noreply.github.com>
2025-06-04 20:59:14 +0200

    adwd

--- engine/render_utils.c
+++ engine/render_utils.c
@@ -6,7 +6,7 @@
 /*   By: alix <alix@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 05:15:00 by aconstan          #+#    #+#             */
-/*   Updated: 2025/06/04 20:57:46 by alix             ###   ########.fr       */
+/*   Updated: 2025/06/04 20:59:07 by alix             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,14 @@ void put_pixel(t_config *conf, int x, int y, int color)
 {
     char *dst;
     
+    // Vérifications complètes
     if (!conf || !conf->win.addr || x < 0 || x >= WIN_WIDTH || y < 0 || y >= WIN_HEIGHT)
         return;
+        
+    // Calcul sécurisé de l'adresse
     dst = conf->win.addr + (y * conf->win.line_len + x * (conf->win.bpp / 8));
+    
+    // Protection supplémentaire
     if (dst >= conf->win.addr && dst < conf->win.addr + (WIN_HEIGHT * conf->win.line_len))
         *(unsigned int*)dst = color;
 }
